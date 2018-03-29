74001004aec9ccd6bf35b5817470ed3533376bb7
Gary Burgmann <garyburgmann@gmail.com>
2018-03-29 17:23:50 +1000

    settings.js fix

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-  GMAPS_KEY: require('./config/gmaps').GMAPS_KEY;
+  GMAPS_KEY: require('./config/gmaps').GMAPS_KEY,
   API_URL: process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:5000/api/'
 }
