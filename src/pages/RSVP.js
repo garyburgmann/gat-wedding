d445dae288c45331ed5f6df1628d83758e548e2f
Gary Burgmann <garyburgmann@gmail.com>
2018-04-03 18:08:05 +1000

    EmailService linked up to send notification of rsvp submission

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -7,7 +7,7 @@ import { Button,
          Form,
          Image,
          Input
-} from 'semantic-ui-react'
+} from 'semantic-ui-react';
 // import {
 //   Form,
 //   StyledText,
@@ -23,24 +23,32 @@ import desktop from '../assets/images/rsvp-desktop.jpg'; // Tell Webpack this JS
 // import mobile from '../assets/images/gat-mobile.jpg'; // Tell Webpack this JS file uses this image
 import seahorses from '../assets/images/seahorses.png'
 import FixedMenu from '../components/Menu';
+import { Redirect } from 'react-router-dom';
+import EmailService from '../services/EmailService';
 
 
 export default class ExampleForm extends Component {
 
   state = {
     email: '', 
-    names: ''
+    names: '',
+    specialDietary: '',
+    seafood: false,
+    noSeafood: false,
+    vegetarian: false,
+    extraNotes: '',
+    attendance: null,
+    warn: false,
+    success: false,
+    redirect: false
   }
 
-  party = [
-    { key: 1, text: '1', value: 1 },
-    { key: 2, text: '2', value: 2 },
-  ]
+  emailService = new EmailService();
 
   handleAttendance = (e, { value }) => {
     // console.log(value, this .state);
 
-    this.setState({ attendance: value });
+    this.setState({ attendance: value == 'true' });
     setTimeout(() => {
       console.log(value, this.state);
     }, 10);
@@ -53,9 +61,47 @@ export default class ExampleForm extends Component {
       console.log(this.state);
     }, 1);
   }
+
+  handleFood = (e, { name, value }) => {
+    console.log(name, value);
+    this.setState({ [name]: !this.state[name] });
+    setTimeout(() => {
+      console.log(this.state);
+    }, 1);
+  }
   
-  handleSubmit = () => {
-    const { email } = this.state
+  handleSubmit = async () => {
+    const { attendance, email, names, specialDietary, seafood, noSeafood, vegetarian, extraNotes } = this.state;
+
+    console.log(this.state)
+    if (attendance === null || names.length < 3 || email.length < 3) {
+      // alert('Must set attendance');
+      this.setState({ warn: true })
+      // console.log('WARN: ', this.state.warn);
+      setTimeout(() => {
+        this.setState({ warn: false })
+        // console.log('WARN 2: ', this.state.warn);
+      }, 4000);
+    } else {
+      this.setState({ success: true });
+      const data = {
+        attendance: attendance, 
+        email: email, 
+        names: names, 
+        specialDietary: specialDietary, 
+        seafood: seafood, 
+        noSeafood: noSeafood, 
+        vegetarian: vegetarian,
+        extraNotes: extraNotes
+      }
+
+      const res = await this.emailService.sendRSVP(data);
+      console.log('RES: ', res);
+      setTimeout(() => {
+        this.setState({ redirect: true });
+        // console.log('WARN 2: ', this.state.warn);
+      }, 3000);
+    }
 
     // this.setState({ submittedName: name, submittedEmail: email })
   }
@@ -65,35 +111,53 @@ export default class ExampleForm extends Component {
     this.setState({ mobile: val });
   }
 
+  handleDismiss = () => {
+    this.setState({ warn: false })
+  }
+
   // content = (
     
   // );
 
   render() {
-    const { attendance, email } = this.state
+    const { attendance, names, email, specialDietary, extraNotes, redirect, success, warn } = this.state
     let jedi = false;
+
+    if (redirect) {
+      return (
+        <Redirect to='/' />
+      )
+    }
+
+    if(success) {
+      return (
+        <div style={{textAlign: 'center'}}>
+          <Message success header='Success!' content='Your RSVP has been submitted. Thank you :)' />
+        </div>
+      )
+    }
+
     return (
       <div>
-          <MediaQuery query="(min-device-width: 850px)"> 
-            {(matches) => {
-              if (matches) {
-                jedi = true;
-                if (jedi != this.state.mobile) {
-                  this.handleScreen(jedi);
-                }
-                
-                console.log('JEDI: ', jedi);
-                return null;
-              } else {
-                jedi = false;
-                if (jedi != this.state.mobile) {
-                  this.handleScreen(jedi);
-                }
-                console.log('JEDI: ', jedi);
-                return null;
+        <MediaQuery query="(min-device-width: 850px)"> 
+          {(matches) => {
+            if (matches) {
+              jedi = true;
+              if (jedi != this.state.mobile) {
+                () => this.handleScreen(jedi);
               }
-            }}
-          </MediaQuery>
+              console.log('JEDI: ', jedi);
+              return null;
+            } else {
+              jedi = false;
+              if (jedi != this.state.mobile) {
+                () => this.handleScreen(jedi);
+              }
+              console.log('JEDI: ', jedi);
+              return null;
+            }
+          }}
+        </MediaQuery>
         <Segment
             inverted
             style={{minHeight: '100vh', padding: '1em 0em', background: `#000 url('${desktop}') no-repeat center center `, backgroundSize: 'cover', }}
@@ -106,47 +170,56 @@ export default class ExampleForm extends Component {
               <Grid.Column style={{ maxWidth: this.state.mobile ? 700 : 500 }}>
 
                 <Segment style={{minHeight: '50vh', padding: '1em 0em', background: `#F7F7F7`}} >
-                  <Form onSubmit={this.handleSubmit} size="big">
+                  <Form onSubmit={this.handleSubmit} size="big" error={warn} success={success}>
                     
                     <Header as='h1' textAlign='center' size='huge' style={{fontFamily: `'Allura', cursive`, padding: '1em 0em'}}>
                       Please reply by September 03, 2018
-                    </Header>
-                    
-                  
+                    </Header> 
+
                     <div style={{textAlign: 'center', marginBottom: '2em', fontFamily: `'Libre Baskerville', serif`}}>
-                      <Form.Input placeholder='Your names' name='names' transparent onChange={this.handleInput}
+                      <Form.Input placeholder='Your names' name='names' value={names} transparent onChange={this.handleInput} 
                                   style={{width: '80%', borderStyle: 'solid', borderWidth: '0px 0px 1px 0px', borderColor: 'teal', margin: '0 0 1em 0'}} />
                     </div>
 
                     <div style={{textAlign: 'center', marginBottom: '2em', fontFamily: `'Libre Baskerville', serif`}}>
-                      <Form.Input placeholder='Your email' name='email' transparent type='email' onChange={this.handleInput}
+                      <Form.Input placeholder='Your email' name='email' value={email} transparent type='email' onChange={this.handleInput}
                                   style={{width: '80%', borderStyle: 'solid', borderWidth: '0px 0px 1px 0px', borderColor: 'teal', margin: '0 0 1em 0'}} />
                     </div>
                     
                     <div style={{ marginBottom: '2em', fontFamily: `'Libre Baskerville', serif` }}>
-                      <Form.Group grouped style={{marginLeft: '10%'}}>
+                      <Form.Group grouped style={{marginLeft: '10%'}} >
                         {/* <label>Can you attend?</label> */}
-                        <Form.Checkbox type='radio' label='Accept' value='yes' checked={attendance === 'yes'} onChange={this.handleAttendance} />
-                        <Form.Checkbox type='radio' label='Decline' value='no' checked={attendance === 'no'} onChange={this.handleAttendance} />
+                        <Form.Checkbox type='radio' name='attendance' label='Accept' value='true' checked={attendance === true} onChange={this.handleAttendance} />
+                        <Form.Checkbox type='radio' name='attendance' label='Decline' value='false' checked={attendance === false} onChange={this.handleAttendance} />
                       </Form.Group>
                     </div>
                     
                     <div style={{marginLeft: '10%', marginBottom: '2em', fontFamily: `'Libre Baskerville', serif`}}>
                       <Form.Group grouped>
-                        <Form.Checkbox label='I love seafood!' />
-                        <Form.Checkbox label="I don't eat fish or seafood" />
-                        <Form.Checkbox label="I'm vegetarian" />
+                        <Form.Checkbox label='I love seafood!' name='seafood' onChange={this.handleFood} />
+                        <Form.Checkbox label="I don't eat fish or seafood" name='noSeafood' onChange={this.handleFood} />
+                        <Form.Checkbox label="I'm vegetarian" name='vegetarian' onChange={this.handleFood} />
                       </Form.Group>
                     </div>
 
                     <div style={{textAlign: 'center', marginBottom: '2em', fontFamily: `'Libre Baskerville', serif`}}>
-                      <Form.Input placeholder='Other dietary requirements?' name='dietary' transparent 
+                      <Form.Input placeholder='Other dietary requirements?' name='specialDietary' value={specialDietary} transparent onChange={this.handleInput}
+                                  style={{width: '80%', borderStyle: 'solid', borderWidth: '0px 0px 1px 0px', borderColor: 'teal', margin: '0 0 1em 0'}} />
+                    </div>
+
+                    <div style={{textAlign: 'center', marginBottom: '2em', fontFamily: `'Libre Baskerville', serif`}}>
+                      <Form.Input placeholder='Extra notes' name='extraNotes' value={extraNotes} transparent onChange={this.handleInput}
                                   style={{width: '80%', borderStyle: 'solid', borderWidth: '0px 0px 1px 0px', borderColor: 'teal', margin: '0 0 1em 0'}} />
                     </div>
 
                     <div style={{textAlign: 'center', marginBottom: '2em', fontFamily: `'Libre Baskerville', serif`}}>
                     {/* <Form.TextArea label='Any other comments or information?' placeholder='Please enlighten us...' /> */}
-                      <Form.Button>Submit</Form.Button>
+                      <Form.Button color='teal' type='submit'>Submit</Form.Button>
+
+                      <Message error header='Missing Info' content='We need your names, email and attendance please.' />
+
+                      <Message success header='Success!' content='Your RSVP has been submitted. Thank you :)' />
+
                       <Image src={seahorses} size='small' centered style={{padding: '1em 0em'}}/>
                     </div>
                    
---
+++ src/services/EmailService.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import {API_URL} from '../settings';
+
+
+class EmailService {
+
+  apiUrl = API_URL;
+
+  sendRSVP = (form, subject = 'RSVP - GAT WEDDING') => {
+    const url = `${this.apiUrl}email`;
+
+    const attendanceString = form.attendance ? 
+      `I/We look forward to coming along to help celebrate your big day. <br /> ` : 
+      `So sorry but we won't be able to make it along for your big day. <br /> <br />`; 
+    
+    let textString = `<h1>New RSVP Submitted for Gat Wedding :) </h1> <br />
+                      <p>Dear Gary and Kat, 
+                      <br /> <br /> 
+                        ${attendanceString}`;
+    
+    if (form.attendance) {
+      const seafood = form.seafood ? 
+        `I/We love eating seafood!` :
+        `No seafood for me/us thanks.`;
+
+      const noSeafood = form.noSeafood ?
+        `I/We don't eat seafood at all.` :
+        `I/We will eat seafood.`;
+
+      const vegetarian = form.vegetarian ? 
+        `I/We are vegetarian too like that totally awesome dude Gary.` :
+        `I/We are meatosaruses.`;
+
+      const specialDietary = `I/we require: ${form.specialDietary}`;
+
+      textString += `<ul> 
+                       <li> ${seafood} </li>
+                       <li> ${noSeafood} </li>
+                       <li> ${vegetarian} </li>
+                       <li> ${specialDietary} </li>
+                     </ul>
+                     <br />`;
+
+    }
+
+    const extraNotes = `<b>Extra notes: </b> ${form.extraNotes}
+                        <br /> <br />`;
+
+    textString += extraNotes;
+
+    textString += `Kind regards,
+                   <br /> 
+                   ${form.names}`;
+
+    const addresses = ['gatpad5@gmail.com', form.email];
+       
+    const data = {
+      to: addresses,
+      subject: subject, 
+      text: textString
+    }
+
+    return axios.post(url, data)
+      .then((res) => {
+        console.log('EMAIL RES: ', res.data)
+        return res;
+      })
+      .catch((err) => {
+        console.log("EMAIL ERR: ", err);
+        return err;
+      });
+
+  }
+}
+
+export default EmailService;
